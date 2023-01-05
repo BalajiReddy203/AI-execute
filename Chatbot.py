106ff9be8d8519c193bfa66774044795d64baac3
BalajiReddy203 <109952927+BalajiReddy203@users.noreply.github.com>
2023-01-05 08:24:26 +0530

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -1,20 +1,28 @@
-pairs=[
-	["My name is(.*)",["Hello %1,How are you today"]],
-	["(.*)help",["yes,I can help you",]],
-	["(.*)your name?",["my name is swarna,i am a chatbot",]],
-	["how are you(.*)?",["i am great"]],
-	["sorry(.*)",["its ok, never mind that",]],
-	["(hi|hey|hello|hola|holla)(.*)",["hello","hey there","hi"]],
-	["(.*)not feeling (.*)",["ok,tell me your problem",]],
-	["(.*)created(.*)",["Mmvc created me using pythons nltk library","top secret:)",]],
-        ["(.*)cough(.*)",["take ginger tea and take cough syrup",]],
-        ["(.*)(headache/fever)(.*)",["please take paracetamol tablet after food",]],
-        ["(.*)stomach(.*)",["please take lightweight food",]],
-        ["quit",["bye for now,see you soon ,it was nice talking to you...:)"]],
-        ]
-from nltk.chat.util import Chat,reflections
-print("hi,i am swarna and i am chat bot\n please type lowercase english language to start a conversion \n type quit to leave")
-#create chat bot
-chat=Chat(pairs,reflections)
-#start coversation
-chat.converse()
+print("Simple Question and Answering Program")
+print("=====================================")
+print(" You may ask any one of these questions")
+print("Hi")
+print("How are you?")
+print("Are you working?")
+print("What is your name?")
+print("what did you do yesterday?")
+print("Quit")
+while True:
+    question = input("Enter one question from above list:")
+    if question in ['hi']:
+        print("Hello")
+    elif question in ['how are you?','how do you do?']:
+        print("I am fine")
+    elif question in ['are you working?','are you doing any job?']:
+        print("yes. I'am working in AITS-R")
+    elif question in ['what is your name?']:
+        print("My name is Emilia")
+        name=input("Enter your name?")
+        print("Nice name and Nice meeting you",name)
+    elif question in ['what did you do yesterday?']:
+        print("I saw Raja Rani 5 times")
+    elif question in ['quit']:
+        break
+    else:
+        print("I don't understand what you said")
+
